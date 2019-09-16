e83ed8e420b23baefd91795babaefe0bdcb61a17
Prateeksha Lingashettar <46060795+prateekshal@users.noreply.github.com>
2019-09-16 18:52:32 -0400

    Add files via upload

--- part2.js
+++ part2.js
@@ -6,7 +6,15 @@
  * r*d = s
  * s-e = t
  * t/f = u
- * But it cannot perform operations with more than two operands such as a+b+c-d=*/
+ * But it does not support operations with more than two operands such as a+b+c-d=
+ * Negative numbers are supported.
+ * Once any operator is clicked, the display changes to 0, but it retains the state.
+ * Operations such as a+= are not supported by this calculator.
+ * 
+ * Attribution:
+ * W3Schools - JavaScript.
+ * 
+ * */
 
 
 "use strict";
@@ -14,10 +22,13 @@
 (function(){
     window.onload = function(){
         let op;                 //To store the operator.
-        let firstVal = 0;       //To store the operand 1 and clear the display once any operator is clicked.
+        let firstVal;           //To store the operand 1 and clear the display once any operator is clicked.
         let secondVal;          //To store the operand 2.
+        let negative = false;
         let countOp = 0;        //To check if + or = was clicked.
         let decCount = 0;       //To check if user wants to enter a decimal number.
+        let secNegative = false;
+        let expression;
 
         let display = (val) => {
             let currentDisplayVal = parseFloat(document.getElementById("displayResult").innerText,10);
@@ -40,57 +51,118 @@
 
         document.getElementById("one").onclick = function(){
             let val = document.getElementById("one").innerText;
+            if(expression === undefined){
+                expression = "1";
+            }
+            else{
+                expression += val;
+            }
             display(val);
         }
 
         document.getElementById("two").onclick = function(){
             let val = document.getElementById("two").innerText;
+            if(expression === undefined){
+                expression = "2";
+            }
+            else{
+                expression += val;
+            }
             display(val);
         }
 
         document.getElementById("three").onclick = function(){
             let val = document.getElementById("three").innerText;
+            if(expression === undefined){
+                expression = "3";
+            }
+            else{
+                expression += val;
+            }
             display(val);
         }
 
         document.getElementById("four").onclick = function(){
             let val = document.getElementById("four").innerText;
+            if(expression === undefined){
+                expression = "4";
+            }
+            else{
+                expression += val;
+            }
             display(val);
         }
 
         document.getElementById("five").onclick = function(){
             let val = document.getElementById("five").innerText;
+            if(expression === undefined){
+                expression = "5";
+            }
+            else{
+                expression += val;
+            }
             display(val);
         }
 
         document.getElementById("six").onclick = function(){
             let val = document.getElementById("six").innerText;
+            if(expression === undefined){
+                expression = "6";
+            }
+            else{
+                expression += val;
+            }
             display(val);
         }
 
         document.getElementById("seven").onclick = function(){
             let val = document.getElementById("seven").innerText;
+            if(expression === undefined){
+                expression = "7";
+            }
+            else{
+                expression += val;
+            }
             display(val);
         }
 
         document.getElementById("eight").onclick = function(){
             let val = document.getElementById("eight").innerText;
+            if(expression === undefined){
+                expression = "8";
+            }
+            else{
+                expression += val;
+            }
             display(val);
         }
 
         document.getElementById("nine").onclick = function(){
             let val = document.getElementById("nine").innerText;
+            if(expression === undefined){
+                expression = "9";
+            }
+            else{
+                expression += val;
+            }
             display(val);
         }
 
         document.getElementById("zero").onclick = function(){
             let val = document.getElementById("zero").innerText;
+            if(expression === undefined){
+                expression = "0";
+            }
+            else{
+                expression += val;
+            }
             display(val);
         }
 
         document.getElementById("clear").onclick = function(){
             decCount = 0;
             countOp = 0;
+            expression = undefined;
             document.getElementById("displayResult").innerText = parseInt(0, 10);
         }
 
@@ -99,6 +171,7 @@
                 firstVal = parseFloat(document.getElementById("displayResult").innerText,10);
                 document.getElementById("displayResult").innerText = parseFloat(0, 10);
                 op = "+";
+                expression += "+";
                 countOp++;
                 decCount = 0;
             }
@@ -109,16 +182,28 @@
         }
 
         document.getElementById("sub").onclick = function(){
-            if(countOp === 0){
-                firstVal = parseFloat(document.getElementById("displayResult").innerText,10);
-                document.getElementById("displayResult").innerText = parseFloat(0, 10);
-                op = "-";
-                countOp++;
-                decCount = 0;
+            if(expression === undefined){
+                negative = true;
             }
             else{
-                operation(op);
+                if(countOp === 0){
+                    firstVal = parseFloat(document.getElementById("displayResult").innerText,10);
+                    document.getElementById("displayResult").innerText = parseFloat(0, 10);
+                    op = "-";
+                    expression += "-";
+                    countOp++;
+                    decCount = 0;
+                }
+                else if(op !== undefined){
+                    secNegative = true;
+                }
+                else{
+                    countOp++;
+                    secondVal = parseFloat(document.getElementById("displayResult").innerHTML,10);
+                    operation(op);
+                }
             }
+            
         }
 
         document.getElementById("division").onclick = function(){
@@ -126,6 +211,7 @@
                 firstVal = parseFloat(document.getElementById("displayResult").innerText,10);
                 document.getElementById("displayResult").innerText = parseFloat(0, 10);
                 op = "/";
+                expression += "/";
                 countOp++;
                 decCount = 0;
             }
@@ -140,6 +226,7 @@
                 firstVal = parseFloat(document.getElementById("displayResult").innerText,10);
                 document.getElementById("displayResult").innerText = parseInt(0, 10);
                 op = "*";
+                expression += "*";
                 countOp++;
                 decCount = 0;
             }
@@ -150,9 +237,17 @@
         }
 
         let operation = (operation) => {
+            if(negative){
+                firstVal = firstVal * (-1);
+                negative = false;    
+            }
+            if(secNegative){
+                secondVal = secondVal * (-1);
+                secNegative = false;
+            }
             switch(operation){   
                 case "+":
-                    if(this.isNaN(secondVal) && countOp === 1){
+                    if(isNaN(secondVal) && countOp === 1){
                         document.getElementById("displayResult").innerText = firstVal + firstVal;
                         countOp = 0;
                         decCount = 0;
@@ -161,11 +256,13 @@
                         document.getElementById("displayResult").innerText = firstVal + secondVal;
                         countOp = 0;
                         decCount = 0;
+                        op = undefined;
+                        secondVal = this.undefined;
+                        expression = document.getElementById("displayResult").innerText;
                     }
                     break;
                 case "-":
-                    if(this.isNaN(secondVal) && countOp === 1){
-                        alert("enter if");
+                    if(isNaN(secondVal) && countOp === 1){
                         document.getElementById("displayResult").innerText = firstVal - firstVal;
                         countOp = 0;
                         decCount = 0;
@@ -174,6 +271,9 @@
                         document.getElementById("displayResult").innerText = firstVal - secondVal;
                         countOp = 0;
                         decCount = 0;
+                        op = undefined;
+                        expression = document.getElementById("displayResult").innerText;
+                        secondVal = undefined;
                     }
                     break;
                 case "*":
@@ -186,6 +286,9 @@
                         document.getElementById("displayResult").innerText = firstVal * secondVal;
                         countOp = 0;
                         decCount = 0;
+                        op=this.undefined;
+                        secondVal = this.undefined;
+                        expression = document.getElementById("displayResult").innerText;
                     }
                     break;
                 case "/":
@@ -198,6 +301,9 @@
                         document.getElementById("displayResult").innerText = firstVal / secondVal;
                         countOp = 0;
                         decCount = 0;
+                        secondVal = this.undefined;
+                        op = this.undefined;
+                        expression = document.getElementById("displayResult").innerText;
                     }
                     break;
                 default:
